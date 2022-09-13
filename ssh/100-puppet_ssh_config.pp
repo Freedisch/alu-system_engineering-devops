1781cfd5468a8d0cbc84ac45d731421c9fbb4802
Freedisch <freeproduc@gmail.com>
2022-09-13 02:27:38 +0200

    100-puppet_ssh_config.pp

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+class { 'ssh':
+  storeconfigs_enabled => false,
+  server_options => {
+    'Match User www-data' => {
+      'ChrootDirectory' => '%h',
+      'ForceCommand' => 'internal-sftp',
+      'PasswordAuthentication' => 'no',
+      'IdentityFile' => '~/.ssh/school',
+      'AllowTcpForwarding' => 'no',
+      'X11Forwarding' => 'no',
+    },
+    'Port' => [22, 2222, 2288],
+  },
