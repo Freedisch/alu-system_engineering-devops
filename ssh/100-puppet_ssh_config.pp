c8d68ab8f8a792d58e66af734cbce34cff980e1c
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-09-13 02:35:50 +0200

    no password

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
 #Puppet is config file for ssh files
-class { 'ssh':
-  storeconfigs_enabled => false,
-  server_options => {
-    'Match User www-data' => {
-      'ChrootDirectory' => '%h',
-      'ForceCommand' => 'internal-sftp',
-      'PasswordAuthentication' => 'no',
-      'IdentityFile' => '~/.ssh/school',
-      'AllowTcpForwarding' => 'no',
-      'X11Forwarding' => 'no',
-    },
-    'Port' => [22, 2222, 2288],
-  },
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
